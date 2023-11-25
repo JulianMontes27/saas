fa494c86b34d752b57f3ed371ccc2972e1b17300
Julian Montes <julianmontesPS4@gmail.com>
2023-11-25 17:45:19 -0500

    skeleton of the form done
    Now working on the api calls and messages display

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -1,21 +1,88 @@
+"use client";
+
 import React from "react";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import Heading from "@/components/non-shadcn/Heading";
 import { Code } from "lucide-react";
+import { formSchema } from "./constants";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const CodePage = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema), // validation
+    defaultValues: {
+      prompt: "",
+    },
+  });
+  // loading state
+  const isLoading = form.formState.isSubmitting;
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div>
       <Heading
         title="Code Generation"
         description="Our most advanced code generation model"
         icon={Code}
-        iconColor= "text-green-700"
-        bgColor= "bg-green-700/10"
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
       />
       <div className="px-4 lg:px-8">
-        hello world!
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+          >
+            <FormField
+              control={form.control}
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-12 lg:col-span-10">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      placeholder="What does console.log() mean in JS?"
+                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="col-span-12 lg:col-span-2 w-full"
+              disabled={isLoading}
+            >
+              Generate
+            </Button>
+          </form>
+        </Form>
       </div>
+      <div className="space-y-4 mt-4">Messages content</div>
     </div>
   );
 };
